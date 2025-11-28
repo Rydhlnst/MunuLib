173bbcc5bbe9d0ac631a50c58b79d68fc0c2ccd6
Rydhlnst <rydhlnst@gmail.com>
2025-11-28 08:12:16 +0700

    AuthClient Fixed

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,5 +1,6 @@
 import { createAuthClient } from "better-auth/react"
+import config from "./config"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000"
+    baseURL: config.env.apiEndpoint
 })
